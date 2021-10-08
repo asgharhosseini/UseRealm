c8e1d3024374dae17e89a8e50304853e2631013a
asghar hosseini <asgharhosseini.v@gmail.com>
2021-10-08 15:38:42 +0330

    add UpdateUser and add getAllUser and add getUsersByName and add deleteAll and add deleteByEmail

--- app/src/main/java/ir/ah/app/UseRealm/model/UserDAO.java
+++ app/src/main/java/ir/ah/app/UseRealm/model/UserDAO.java
@@ -11,45 +11,86 @@ public class UserDAO {
     public UserDAO() {
         realm = Realm.getDefaultInstance();
     }
-    public void saveUser(User user){
+
+    public void saveUser(User user) {
         realm.executeTransactionAsync(new Realm.Transaction() {
             @Override
             public void execute(Realm realm) {
-                User realmObject =realm.createObject(User.class,user.getEmail());
+                User realmObject = realm.createObject(User.class, user.getEmail());
                 realmObject.setName(user.getName());
 
 
             }
-        },new Realm.Transaction.OnSuccess(){
+        }, new Realm.Transaction.OnSuccess() {
 
             @Override
             public void onSuccess() {
-                Log.e("REALM_TAG","Success");
+                Log.e("REALM_TAG", "Success");
 
             }
-        },new Realm.Transaction.OnError(){
+        }, new Realm.Transaction.OnError() {
 
             @Override
             public void onError(Throwable error) {
-                Log.e("REALM_TAG","error");
-                Log.e("REALM_TAG","error :"+error.getMessage());
+                Log.e("REALM_TAG", "error");
+                Log.e("REALM_TAG", "error :" + error.getMessage());
             }
         });
     }
-    public void close(){
+
+    public void UpdateUser(User user){
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                realm.copyToRealmOrUpdate(user);
+            }
+        });
+
+    }
+
+    public void close() {
         realm.close();
     }
 
-    public RealmResults<User>getAllUser(){
-        RealmResults<User> realmResults =realm.where(User.class).findAll();
+    public RealmResults<User> getAllUser() {
+        RealmResults<User> realmResults = realm.where(User.class).findAll();
         return realmResults;
     }
-    public User getUserByEmail(String email){
-        User user=realm.where(User.class).equalTo("email",email).findFirst();
+    public RealmResults<User> getAllUser(String email,String name) {
+        RealmResults<User> realmResults =
+                realm.where(User.class)
+                        .equalTo("email",email)
+                        .or()
+                        .equalTo("name",name)
+                        .findAll();
+        return realmResults;
+    }
+
+    public User getUserByEmail(String email) {
+        User user = realm.where(User.class).equalTo("email", email).findFirst();
         return user;
     }
-    public RealmResults<User> getUsersByName(String name){
-        RealmResults<User> user=realm.where(User.class).equalTo("name",name).findAll();
+
+    public RealmResults<User> getUsersByName(String name) {
+        RealmResults<User> user = realm.where(User.class).equalTo("name", name).findAll();
         return user;
     }
+
+    public void deleteAll() {
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                getAllUser().deleteAllFromRealm();
+            }
+        });
+    }
+    public void deleteByEmail(String email){
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                getUserByEmail(email).deleteFromRealm();
+            }
+        });
+    }
+
 }
